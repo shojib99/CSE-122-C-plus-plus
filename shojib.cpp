fe307bce0cf2e8c1d0c23c5c4bd0b2da82b58a85
Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2023-07-30 15:02:32 +0600

    Add files via upload

---
+++ shojib.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Hello in my university"<<endl;
+cout<<endl;
+cout<<"I am a student in it faculty"<<endl;
+cout<<endl;
+cout<<"Welcome"<<endl;
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
