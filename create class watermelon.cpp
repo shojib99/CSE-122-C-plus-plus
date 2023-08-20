a94155dd0de677e06973638261a131da499f841e
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2023-08-20 16:14:59 +0600

    Update create class watermelon.cpp

--- create class watermelon.cpp
+++ create class watermelon.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class watermelon{
+
+public:
+    void disply (int n)
+    {
+        if(n%2==0 && n>2)
+        {
+            cout<< "YES";
+        }
+        else
+        {
+           cout<< "NO";
+        }
+    }
+
+};
+
+int main()
+{
+    int n;
+    cin>> n;
+    watermelon ob1;
+    ob1.disply(n);
+}
