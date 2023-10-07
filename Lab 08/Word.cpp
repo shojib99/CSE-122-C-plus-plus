1c2fde4b2bd89faaffa33423793da79d47894ade
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2023-10-07 19:58:33 +0600

    Add files via upload

---
+++ Lab 08/Word.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int upp =0,low =0;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.size(); i++)
+    {
+        if( isupper(s[i]))
+        {
+            upp++;
+        }
+        else
+        {
+            low++;
+        }
+    }
+    if(upp<low)
+    {
+        for(int i=0; i<s.size(); i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        cout<<s<<endl;
+    }
+
+    if(upp>low)
+    {
+        for(int i=0; i<s.size(); i++)
+        {
+            s[i]=toupper(s[i]);
+        }
+        cout<<s<<endl;
+    }
+
+   if(upp==low)
+    {
+        for(int i=0; i<s.size(); i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        cout<<s<<endl;
+    }
+}
+
+
+
+
+
+
